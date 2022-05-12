32e6de78aed579bde3f0ed4af0077f3b51f42841
Francisco Sicardi <francisco.sicardi@mercadolibre.com>
2022-05-12 19:03:16 -0300

    unify firebase

--- src/others/constants.js
+++ src/others/constants.js
@@ -43,27 +43,14 @@ const JSON_HEADER = {
 const isDevelopment = process.env
                              .REACT_APP_PRODUCTION === "false";
 
-let firebaseConfig;
-
-if (isDevelopment) {
-  firebaseConfig = {
-    apiKey: "AIzaSyDlFbw1n3eqg7ogdwGuiTetV6isK4Uhqno",
-    authDomain: "fir-firebase-acc6b.firebaseapp.com",
-    projectId: "fir-firebase-acc6b",
-    storageBucket: "fir-firebase-acc6b.appspot.com",
-    messagingSenderId: "296878360901",
-    appId: "1:296878360901:web:7987ce42ec0a406b1f162c"
-  };
-} else {
-  firebaseConfig = {
-    apiKey: "AIzaSyCnDa9J7DKKtNv5crxZ4NrRGcW5c7nZTAg",
-    authDomain: "fir-firebase-2-9eb22.firebaseapp.com",
-    projectId: "fir-firebase-2-9eb22",
-    storageBucket: "fir-firebase-2-9eb22.appspot.com",
-    messagingSenderId: "701624425016",
-    appId: "1:701624425016:web:6cb2157c5a2c0a34e1a4cd"
-  };
-}
+const firebaseConfig = {
+  apiKey: "AIzaSyCnDa9J7DKKtNv5crxZ4NrRGcW5c7nZTAg",
+  authDomain: "fir-firebase-2-9eb22.firebaseapp.com",
+  projectId: "fir-firebase-2-9eb22",
+  storageBucket: "fir-firebase-2-9eb22.appspot.com",
+  messagingSenderId: "701624425016",
+  appId: "1:701624425016:web:6cb2157c5a2c0a34e1a4cd"
+};
 
 const PASSWORD_MIN_LEN = 10;
 
